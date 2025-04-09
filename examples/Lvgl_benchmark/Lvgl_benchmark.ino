126e51a5dcf24b4b2ecf754e69ad8a5748ecd338
LILYGO_L <13073925292@163.com>
2025-04-09 09:13:52 +0800

    Update Lvgl_benchmark example #4

---
+++ examples/Lvgl_benchmark/Lvgl_benchmark.ino
@@ -0,0 +1,109 @@
+/*
+ * @Description: None
+ * @Author: LILYGO_L
+ * @Date: 2025-04-09 09:02:08
+ * @LastEditTime: 2025-04-09 09:09:27
+ * @License: GPL 3.0
+ */
+
+#include "lvgl.h"
+#include "Arduino_GFX_Library.h"
+#include "pin_config.h"
+#include "lv_demo_benchmark.h"
+
+Arduino_DataBus *bus = new Arduino_SWSPI(-1 /* DC */, LCD_CS /* CS */,
+                                         SCLK /* SCK */, MOSI /* MOSI */, -1 /* MISO */);
+Arduino_ESP32RGBPanel *rgbpanel = new Arduino_ESP32RGBPanel(
+    -1 /* DE */, LCD_VSYNC /* VSYNC */, LCD_HSYNC /* HSYNC */, LCD_PCLK /* PCLK */,
+    LCD_B0 /* B0 */, LCD_B1 /* B1 */, LCD_B2 /* B2 */, LCD_B3 /* B3 */, LCD_B4 /* B4 */,
+    LCD_G0 /* G0 */, LCD_G1 /* G1 */, LCD_G2 /* G2 */, LCD_G3 /* G3 */, LCD_G4 /* G4 */, LCD_G5 /* G5 */,
+    LCD_R0 /* R0 */, LCD_R1 /* R1 */, LCD_R2 /* R2 */, LCD_R3 /* R3 */, LCD_R4 /* R4 */,
+    1 /* hsync_polarity */, 20 /* hsync_front_porch */, 2 /* hsync_pulse_width */, 0 /* hsync_back_porch */,
+    1 /* vsync_polarity */, 30 /* vsync_front_porch */, 8 /* vsync_pulse_width */, 1 /* vsync_back_porch */,
+    10 /* pclk_active_neg */, 6000000L /* prefer_speed */, false /* useBigEndian */,
+    0 /* de_idle_high*/, 0 /* pclk_idle_high */);
+Arduino_RGB_Display *gfx = new Arduino_RGB_Display(
+    LCD_WIDTH /* width */, LCD_HEIGHT /* height */, rgbpanel, 0 /* rotation */, true /* auto_flush */,
+    bus, -1 /* RST */, st7701_type9_init_operations, sizeof(st7701_type9_init_operations));
+
+static lv_disp_draw_buf_t draw_buf;
+static lv_disp_drv_t disp_drv;
+
+/* Display flushing */
+void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
+{
+    uint32_t w = (area->x2 - area->x1 + 1);
+    uint32_t h = (area->y2 - area->y1 + 1);
+
+#if (LV_COLOR_16_SWAP != 0)
+    gfx->draw16bitBeRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
+#else
+    gfx->draw16bitRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
+#endif
+
+    lv_disp_flush_ready(disp);
+}
+
+/*Read the touchpad*/
+void my_touchpad_read(lv_indev_drv_t *indev_drv, lv_indev_data_t *data)
+{
+}
+
+void lvgl_initialization(void)
+{
+    lv_init();
+
+    uint32_t screen_width = gfx->width();
+    uint32_t screen_height = gfx->height();
+
+    // 使用PSRAM分配内存
+    lv_color_t *buf_1 = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * LCD_WIDTH * 40, MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
+    lv_color_t *buf_2 = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * LCD_WIDTH * 40, MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
+
+    while ((!buf_1) || (!buf_2))
+    {
+        Serial.println("LVGL disp_draw_buf allocate failed!");
+        delay(1000);
+    }
+
+    lv_disp_draw_buf_init(&draw_buf, buf_1, buf_2, LCD_WIDTH * 40);
+
+    /* Initialize the display */
+    lv_disp_drv_init(&disp_drv);
+    /* Change the following line to your display resolution */
+    disp_drv.hor_res = screen_width;
+    disp_drv.ver_res = screen_height;
+    disp_drv.flush_cb = my_disp_flush;
+    disp_drv.draw_buf = &draw_buf;
+    disp_drv.full_refresh = 1; // 双缓冲全像素刷新
+    lv_disp_drv_register(&disp_drv);
+
+    /*Initialize the (dummy) input device driver*/
+    static lv_indev_drv_t indev_drv;
+    lv_indev_drv_init(&indev_drv);
+    indev_drv.type = LV_INDEV_TYPE_POINTER;
+    indev_drv.read_cb = my_touchpad_read;
+    lv_indev_drv_register(&indev_drv);
+}
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("Ciallo");
+
+    pinMode(LCD_BL, OUTPUT);
+    digitalWrite(LCD_BL, HIGH);
+
+    gfx->begin();
+    gfx->fillScreen(BLACK);
+    
+    lvgl_initialization();
+
+    lv_demo_benchmark();
+}
+
+void loop()
+{
+    lv_timer_handler(); /* let the GUI do its work */
+    delay(5);
+}
